6e77a2f5a53df573f7bd07fda3c525740a2f8e5f
hqia <hqia@sina.com>
2017-12-08 21:35:59 +0800

    add find object by text or ID

--- app/src/androidTest/java/com/example/hqia/uiautomatordemo/MyTest.java
+++ app/src/androidTest/java/com/example/hqia/uiautomatordemo/MyTest.java
@@ -7,13 +7,13 @@ import android.os.RemoteException;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.os.SystemClock.sleep;
 
 /**
  * Created by Hqia on 2017/12/07.
@@ -23,8 +23,8 @@ public class MyTest {
     private UiDevice myuidevice = null;
     private Context mycontext = null;
     private Instrumentation myinstrumentation = null;
-//    String testAPP = "com.example.hqia.myapplication";
-    String testAPP = "com.android.settings";
+    String testAPP = "com.example.hqia.myapplication";
+//    String testAPP = "com.android.settings";
 
     @Before
     public void ready() throws RemoteException {
@@ -40,15 +40,15 @@ public class MyTest {
     }
 
     @Test
-    public void test1() throws RemoteException {
+    public void test1() throws RemoteException, UiObjectNotFoundException {
         // 启动测试App
         Intent myintent = mycontext.getPackageManager().getLaunchIntentForPackage(testAPP);
         mycontext.startActivity(myintent);
         myuidevice.waitForWindowUpdate(testAPP, 10000);
-/*        sleep(3000);
-        for (char i = 0; i < 3; i++) {
-            myuidevice.pressRecentApps();
-            sleep(8000);
-        }*/
+        //sleep(3000);
+        UiObject SEND = myuidevice.findObject(new UiSelector().text("SEND"));  //定位text内容为Send的控键
+        SEND.click();  //点击SEND
+        UiObject SEND2 = myuidevice.findObject(new UiSelector().resourceId("com.example.hqia.myapplication:id/button2"));
+        SEND2.click();
     }
 }
